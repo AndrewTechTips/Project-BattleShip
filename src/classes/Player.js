eb505a561e9cba2a778cd873b5b73dae70284784
AndrewTechTips <andreww.steel69@gmail.com>
2026-04-05 11:46:33 +0300

    add Player class with attack and random attack methods

---
+++ src/classes/Player.js
@@ -0,0 +1,30 @@
+import Gameboard from "./Gameboard";
+
+export default class Player {
+    constructor(name, type = "real") {
+        this.name = name;
+        this.type = type;   // real or computer
+        this.Gameboard = new Gameboard();
+    }
+
+    // player attack
+
+    attack(enemyBoard, row, col) {
+        return enemyBoard.receiveAttack(row, col);
+    }
+
+    // logic foc computer , choose a random move
+
+    randomAttack(enemyBoard) {
+        if (this.type !== "computer") return;
+
+        let row, col , result;
+        do {
+            row = Math.floor(Math.random() * 10);
+            row = Math.floor(Math.random() * 10);
+            result = enemyBoard.receiveAttack(row, col);
+        } while (result === "already_attacked");
+
+        return { row, col, result };
+    }
+}
